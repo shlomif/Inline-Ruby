b008a15c164211745d0f0f1010cc4d76a8ff86f8
Shlomi Fish <shlomif@shlomifish.org>
2012-08-09 22:45:51 +0300

    Add Shlomif as maintainer and bump version.

--- Ruby.pm
+++ Ruby.pm
@@ -9,7 +9,7 @@ require DynaLoader;
 require Exporter;
 use vars qw(@ISA $VERSION @EXPORT_OK);
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 @ISA = qw(Inline DynaLoader Exporter);
 @EXPORT_OK = qw(rb_eval
 		rb_call_function
