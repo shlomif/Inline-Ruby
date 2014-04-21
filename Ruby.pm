7db4867b1cceba32c78f1c76159de35025db7916
Shlomi Fish <shlomif@shlomifish.org>
2014-04-21 21:14:04 +0300

    Bump version number.

--- Ruby.pm
+++ Ruby.pm
@@ -9,7 +9,7 @@ require DynaLoader;
 require Exporter;
 use vars qw(@ISA $VERSION @EXPORT_OK);
 
-$VERSION = '0.04';
+$VERSION = '0.05';
 @ISA = qw(Inline DynaLoader Exporter);
 @EXPORT_OK = qw(rb_eval
 		rb_call_function
